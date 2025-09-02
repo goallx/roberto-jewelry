e28d72592abcec3d35e076960af0b5b18ab08c4f
wael.haddad <team@goallx.com>
2025-09-02 22:41:15 +0300

    done

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,34 +1,34 @@
-// app/collections/page.tsx
 import { IProduct } from "@/stores/ProductStore";
 import { getProducts } from "./action";
 import CollectionsDisplay from "./components/CollectionsDisplay";
 
 type CollectionsPageProps = {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 export default async function CollectionsPage({
   searchParams,
 }: CollectionsPageProps) {
+  const params = await searchParams
   const filters = {
     category_name:
-      typeof searchParams.category === "string"
-        ? searchParams.category
+      typeof params.category === "string"
+        ? params.category
         : undefined,
     gender:
-      typeof searchParams.gender === "string" ? searchParams.gender : undefined,
-    minPrice: searchParams.minPrice
-      ? Number(searchParams.minPrice)
+      typeof params.gender === "string" ? params.gender : undefined,
+    minPrice: params.minPrice
+      ? Number(params.minPrice)
       : undefined,
-    maxPrice: searchParams.maxPrice
-      ? Number(searchParams.maxPrice)
+    maxPrice: params.maxPrice
+      ? Number(params.maxPrice)
       : undefined,
     search:
-      typeof searchParams.search === "string" ? searchParams.search : undefined,
-    material: searchParams.material
-      ? Array.isArray(searchParams.material)
-        ? searchParams.material
-        : [searchParams.material]
+      typeof params.search === "string" ? params.search : undefined,
+    material: params.material
+      ? Array.isArray(params.material)
+        ? params.material
+        : [params.material]
       : undefined,
   };
 
@@ -44,8 +44,8 @@ export default async function CollectionsPage({
     <div className="bg-[#FAF8F6] min-h-screen">
       <div className="px-4 md:px-10 pt-40 pb-0">
         <h1 className="text-3xl md:text-4xl font-amandine font-semibold text-center mb-3">
-          {typeof searchParams.category === "string"
-            ? searchParams.category
+          {typeof params.category === "string"
+            ? params.category
             : "Collection Name"}
         </h1>
         <p className="text-center text-base md:text-lg text-gray-600 mb-6 max-w-2xl mx-auto">
